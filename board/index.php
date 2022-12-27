841be5268ed1e9744fe985637727e6c4f09f6b63
mumeinosato <mumeinosato@gmail.com>
2022-12-27 19:06:23 +0900

    test

--- board/index.php
+++ board/index.php
@@ -6,27 +6,16 @@ $db = DB_NAME;
 $user = DB_USER;
 $pass = DB_PASS;
 
-// タイムゾーン設定
-date_default_timezone_set('Asia/Tokyo');
-
-// 変数の初期化
-$current_date = null;
-$message = array();
-$message_array = array();
-$error_message = array();
-$stmt = null;
-$res = null;
-
 session_start();
 
 // データベースに接続
 try {
 
     $option = array(
-        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-        \PDO::MYSQL_ATTR_MULTI_STATEMENTS => false
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_MULTI_STATEMENTS => false
     );
-    $pdo = new \PDO('mysql:charset=UTF8;dbname='.DB_NAME.';host='.DB_HOST , DB_USER, DB_PASS, $option);
+    $pdo = new PDO('mysql:charset=UTF8;dbname='.DB_NAME.';host='.DB_HOST , DB_USER, DB_PASS, $option);
 
 } catch(PDOException $e) {
 
