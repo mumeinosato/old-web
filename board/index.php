b510a72a85c070483aebce705be414f425b04aaa
mumeinosato <mumeinosato@gmail.com>
2022-12-27 19:00:50 +0900

    test

--- board/index.php
+++ board/index.php
@@ -14,10 +14,8 @@ $current_date = null;
 $message = array();
 $message_array = array();
 $error_message = array();
-$pdo = null;
 $stmt = null;
 $res = null;
-$option = null;
 
 session_start();
 
@@ -26,8 +24,7 @@ try {
 
     $option = array(
         \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-        \PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
-        \PDO::ATTR_EMULATE_PREPARES => false
+        \PDO::MYSQL_ATTR_MULTI_STATEMENTS => false
     );
     $pdo = new \PDO('mysql:charset=UTF8;dbname='.DB_NAME.';host='.DB_HOST , DB_USER, DB_PASS, $option);
 
