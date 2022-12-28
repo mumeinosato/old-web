97a5b559959aeddce8af1cb9f5d1ce9d8668f600
mumeinosato <mumeinosato@gmail.com>
2022-12-28 18:54:05 +0900

    404

--- top.php
+++ top.php
@@ -22,7 +22,7 @@
                         <li><a href="/index.php" class="nav-link px-2 text-white">トップページ</a></li>
                         <li><a href="/blog" class="nav-link px-2 text-white">ブログ</a></li>
                         <li><a href="#" class="nav-link px-2 text-white">提供サービス</a></li>
-                        <li><a href="/board" class="nav-link px-2 text-white">掲示板</a></li>
+                        <!-- <li><a href="/board" class="nav-link px-2 text-white">掲示板</a></li> -->
                         <li><a href="#" class="nav-link px-2 text-white">お問い合わせ</a></li>
                     </ui>
                     <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
