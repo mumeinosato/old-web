77dcb0ba02ec94b8bc9df912a91823354363e150
mumeinosato <mumeinosato@gmail.com>
2022-12-28 19:04:30 +0900

    css

--- login/index.php
+++ login/index.php
@@ -23,9 +23,10 @@ session_destroy();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログイン</title>
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sign-in/">
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="signin.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <!-- <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="signin.css" rel="stylesheet"> -->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
