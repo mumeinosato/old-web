8c92113098262126c308d2a8ff691940e072652a
mumeinosato <mumeinosato@gmail.com>
2022-12-28 22:18:27 +0900

    mode

--- darkmode.js
+++ darkmode.js
@@ -1,20 +1,5 @@
-function addDarkmodeWidget() {
-    new Darkmode().showWidget();
-  }
-  window.addEventListener('load', addDarkmodeWidget);
-const options = {
-    /*①*/ bottom: '64px', // default: '32px'
-    /*②*/ right: 'unset', // default: '32px'
-    /*③*/ left: '32px', // default: 'unset'
-    /*④*/ time: '1s', // default: '0.3s'
-    /*⑤*/ mixColor: '#fff', // default: '#fff'
-    /*⑥*/ backgroundColor: '#fff',  // default: '#fff'
-    /*⑦*/ buttonColorDark: '#100f2c',  // default: '#100f2c'
-    /*⑧*/ buttonColorLight: '#fff', // default: '#fff'
+new Darkmode({
     saveInCookies: false, // default: true,
     label: '🌓', // default: ''
-    autoMatchOsTheme: true // default: true
-}
-  
-const darkmode = new Darkmode(options);
-darkmode.showWidget();
+    autoMatchOsTheme: true // default: true'
+}).showWidget();
