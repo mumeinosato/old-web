bffb36f120abd2f514537a53c69817b7ba074966
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2023-01-25 00:00:51 +0900

    修正

--- top.php
+++ top.php
@@ -102,7 +102,7 @@ $year = date('Y');
                 </div>
             </div>
         </div>
-        <div class="c" align="right"><p class="text-muted">©︎<?php echo $year; ?> mumeinosato</p></div>
+        <div class="c"><p class="text-muted">©︎<?php echo $year; ?> mumeinosato</p></div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
     </script>
