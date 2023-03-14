89bb1d27febd87bf7992d76cf60e8000a3f28889
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2023-03-14 12:29:01 +0900

    404

--- error/index.php
+++ error/index.php
@@ -4,11 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-        <title>トップページ - NationMC!</title>
+        <!-- <title>トップページ - NationMC!</title> -->
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <?php include '../top.php'; ?>
+        <!-- <?php include '../top.php'; ?> -->
         <section class="page_404">
             <div class="container">
                 <div class="row">	
