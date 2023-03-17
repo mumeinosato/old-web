84dfecb990366ea55291f2c70eefbbfc7681e870
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2023-03-17 18:49:37 +0900

    test

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
 <?php
-// ファイルに書き込むテキスト
-$content = "改行を含む
-テキストを書き込む\n\n";
-$content2 = "テストテキスト";
-
-
-// ディレクトリに書き込み可能か確認
-if( is_writable($path) ) {
-
-	// ファイルを書き込みモードで開く
-	$file_handle = fopen("/httpdocs/data.txt", "w");
-
-	// ファイルへデータを書き込み
-	fwrite( $file_handle, $content);
-	fwrite( $file_handle, $content2);
-
-	// ファイルを閉じる
-	fclose($file_handle);
-}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+// ファイルに書き込むテキスト
+$content = "改行を含む
+テキストを書き込む\n\n";
+$content2 = "テストテキスト";
+
+
+// ディレクトリに書き込み可能か確認
+if( is_writable($path) ) {
+
+	// ファイルを書き込みモードで開く
+	$file_handle = fopen("data.txt", "w");
+
+	// ファイルへデータを書き込み
+	fwrite( $file_handle, $content);
+	fwrite( $file_handle, $content2);
+
+	// ファイルを閉じる
+	fclose($file_handle);
+}
