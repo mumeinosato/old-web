5ca68aea4b181bcfd8a5cbb0624d88796950db01
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2023-03-17 18:40:22 +0900

    test

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
-require_once 'test.php';
+// ファイルに書き込むテキスト
+$content = "改行を含む
+テキストを書き込む\n\n";
+$content2 = "テストテキスト";
+
+
+// ディレクトリに書き込み可能か確認
+if( is_writable($path) ) {
+
+	// ファイルを書き込みモードで開く
+	$file_handle = fopen("data.txt", "w");
+
+	// ファイルへデータを書き込み
+	fwrite( $file_handle, $content);
+	fwrite( $file_handle, $content2);
+
+	// ファイルを閉じる
+	fclose($file_handle);
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
