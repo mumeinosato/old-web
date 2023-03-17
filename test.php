b4d4600dec68f5047f90a86497306c62bd9a297c
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2023-03-17 18:52:16 +0900

    test

--- test.php
+++ test.php
@@ -1,15 +1,18 @@
 <?php
+
 // ファイルに書き込むテキスト
 $content = "改行を含む
 テキストを書き込む\n\n";
 $content2 = "テストテキスト";
 
+// ファイルを保存するディレクトリのパス
+$path = './testt/';
 
 // ディレクトリに書き込み可能か確認
 if( is_writable($path) ) {
 
 	// ファイルを書き込みモードで開く
-	$file_handle = fopen("data.txt", "w");
+	$file_handle = fopen( $path."data.txt", "w");
 
 	// ファイルへデータを書き込み
 	fwrite( $file_handle, $content);
@@ -18,3 +21,5 @@ if( is_writable($path) ) {
 	// ファイルを閉じる
 	fclose($file_handle);
 }
+
+return;
