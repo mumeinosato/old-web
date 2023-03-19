2c9d9929690a776be29738ea8f0c064fa56a2fd5
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2023-03-19 11:05:32 +0900

    test

--- admin/index.php
+++ admin/index.php
@@ -26,6 +26,7 @@ $login_user = $_SESSION['login_user'];
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
     <link href="../css/dashboard.css" rel="stylesheet">
     <link href="../css/style.css" rel="stylesheet">
+    <!-- <link href="footer.css" rel="stylesheet"> -->
     <link rel="icon" href="/image/icon.ico">
 </head>
 <body>
@@ -36,6 +37,5 @@ $login_user = $_SESSION['login_user'];
         </main>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>   
-    <?php include '../footer.php'; ?>
 </body>
 </html>
