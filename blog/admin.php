d80a65756ec576452acce4819bc4cbbcce31b3f4
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2023-03-19 11:49:56 +0900

    admin css

--- blog/admin.php
+++ blog/admin.php
@@ -33,7 +33,7 @@ function h($s) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/features/">
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
-    <link href="../css/dashboard.css" rel="stylesheet">  
+    <link href="../admin/dashboard.css" rel="stylesheet">  
     <link rel="icon" href="/image/icon.ico">  
 </head>
 <body>
