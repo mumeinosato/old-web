9372ec88697029c831c8c5c49aabd8791100778f
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2023-08-07 23:19:00 +0900

    ActivityPub対応のsnsを作る予定

---
+++ sns/index.php
@@ -0,0 +1,22 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <title>SNS(仮)</title>
+    <link rel="icon" href="/image/icon.ico">
+</head>
+
+<body>
+    <?php include '../top.php'; ?>
+    <div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    </div>
+    <?php include '../footer.php'; ?>
+</body>
+
+</html>
